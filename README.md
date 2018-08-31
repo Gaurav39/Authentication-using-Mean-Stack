6d1c96afa708204113ba32c14d2b3e80942899d1
Gaurav Sharma <imgaurav.1134@gmail.com>
2018-08-31 14:35:59 +0530

    Authentication Using Mean Stack

---
+++ README.md
@@ -0,0 +1,9 @@
+##Authentication using Mean Stack
+
+Complete MEAN stack application with authentication using json web tokens
+
+1) Install the dependencies
+2) $ npm install
+3) Run app
+4) $ npm start
+
---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+
+mongoose.connect(config.database, { useMongoClient: true});
+// Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Database '+config.database);
+});
+// Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error '+err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Port Number
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+
+app.get('/', (req, res) => {
+  res.send('invaild endpoint');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port '+port);
+});
